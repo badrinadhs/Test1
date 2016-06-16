935bbeae6f6d7652d8bae7a9308b00a1d3d0f6bf
nadh1 <sbn1@gmail.com>
2016-06-16 08:51:11 +0530

    First commit

---
+++ TestGit/src/com/edu/sbn/TestClient.java
@@ -0,0 +1,16 @@
+package com.edu.sbn;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		System.out.println(add(3,4));
+
+	}
+	
+	public static int add(int a , int b){
+		return a+b;
+		
+	}
+
+}
